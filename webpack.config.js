a956a29dfa7f735c1954feccfbaf524aab5f2b0c
Reza Merzaie <ghrezam97@gmail.com>
2023-06-15 19:39:03 +0430

    Revert "Write test for add function"
    This reverts commit 4edbf2480ece1138002be9374b7c0bde90b36bc8.

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,6 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.jsx?$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
-        use: ['babel-loader'],
-      },
     ],
   },
   plugins: [new HtmlWebpackPlugin(
@@ -33,18 +28,5 @@ module.exports = {
   )],
   optimization: {
     runtimeChunk: 'single',
-  },
-  resolve: {
-    alias: {
-      config$: './configs/app-config.js',
-      react: './vendor/react-master',
-    },
-    extensions: ['.js', '.jsx'],
-    modules: [
-      'node_modules',
-      'bower_components',
-      'shared',
-      '/shared/vendor/modules',
-    ],
   },
 };
