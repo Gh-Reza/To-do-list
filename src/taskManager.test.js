c52e891672dbbfcd9e9c2e3608465768cfa8176b
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-15 10:32:28 -0500

    Added the remove task test

--- src/taskManager.test.js
+++ src/taskManager.test.js
@@ -19,3 +19,42 @@ describe('Add task', () => {
     expect(updateLocalStorage).toHaveBeenCalledWith(tasksList);
   });
 });
+
+describe('Remove task', () => {
+  test('should remove a task from the task list and update the index', () => {
+    const tasksList = [
+      {
+        description: 'task 1',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'task 2',
+        completed: false,
+        index: 2,
+      },
+      {
+        description: 'task 3',
+        completed: false,
+        index: 3,
+      },
+    ];
+
+    taskManager.removeTask(1, tasksList);
+
+    expect(tasksList).toEqual([
+      {
+        description: 'task 1',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'task 3',
+        completed: false,
+        index: 2,
+      },
+    ]);
+
+    expect(updateLocalStorage).toHaveBeenCalledWith(tasksList);
+  });
+});
